e706d93b38f918789821e1a109188d991d31c76e
sofiekhullar <sofie_k@live.se>
2016-03-12 11:20:19 +0100

    Merge UI och sverige

--- js/include/interaction.js
+++ js/include/interaction.js
@@ -1,8 +1,12 @@
-
-
 controls.zoomSpeed = 1;
 controls.panSpeed = 0;
 controls.rotateSpeed = 1;
+
+$(document).ready(function(){
+    $(".nav-tabs a").click(function(){
+        $(this).tab('show');
+    });
+});
 
 
 
