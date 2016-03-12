f5478a47f67238c1e3dadd43fe951a8cfaf9c8b3
sofiekhullar <sofie_k@live.se>
2016-03-12 11:33:08 +0100

    Fungerande ui

--- js/include/interaction.js
+++ js/include/interaction.js
@@ -1,12 +1,3 @@
 controls.zoomSpeed = 1;
 controls.panSpeed = 0;
 controls.rotateSpeed = 1;
-
-$(document).ready(function(){
-    $(".nav-tabs a").click(function(){
-        $(this).tab('show');
-    });
-});
-
-
-
