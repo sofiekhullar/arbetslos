b8abd569121b4643d360783060fb23c04e3e6523
sofiekhullar <sofie_k@live.se>
2016-03-12 16:59:04 +0100

    La till ett diagram

--- js/include/loadData.js
+++ js/include/loadData.js
@@ -1,13 +1,38 @@
 function loadData() {
 
+	$.getJSON("json/ungdomar.json", function(json) {
+   		loadUngdomar(json.dataset);
+	});
+
 	$.getJSON("json/votes.json", function(json) {
    		loadVotes(json.dataset);
 	});
 
 	$.getJSON("json/csn.json", function(json) {
-   		console.log(json); // this will show the info it in firebug console
 		loadCSN(json.dataset);
 	});
+
+	$.getJSON("json/arbetslosa.json", function(json) {
+		loadArbetsloshet(json);
+	});
+}
+
+function loadUngdomar(dataset){
+
+	$.each(dataset.dimension.Region.category.index, function(lan_id, index){
+
+		var befolkning = 0;
+
+		for(i = index*14*4; i < index*14*4 + 14*4; i++ ){
+
+			befolkning += dataset.value[i];
+		}
+
+		getLan(lan_id).ung_befolkning = befolkning;
+
+	});
+
+
 }
 
 function loadVotes(dataset) {
@@ -51,14 +76,70 @@ function loadVotes(dataset) {
 		
 }
 
-function loadCSN() {
+function loadCSN(dataset) {
 
 	$.each(dataset.dimension.Region.category.index, function(lan_id, index) {
 		
 
+		//indexera i stycken om 8 eftersom det finns 8 partier
+		index = index * 256;
+
+	for(i=index; i < index+128; i +=2){
+
+
+		getLan(lan_id).n_man_studieskuld += dataset.value[i];
+		getLan(lan_id).studieskuld_man += dataset.value[i+ 1];
+
+	}
+
+	for(i=index + 128; i < index+256; i +=2){
+
+
+		getLan(lan_id).n_kvinnor_studieskuld += dataset.value[i];
+		getLan(lan_id).studieskuld_kvinnor += dataset.value[i+ 1];
+
+	}
+
+
 
 	});
 
+
 }
+
+
+function loadArbetsloshet(json){
+
+
+	for(i=0; i < 21 ; i++){
+		for(j=0; j < 21; j++){
+
+			if(json.Blad1[i]["län"].localeCompare(alla_lan[j].namn)==0){
+
+				alla_lan[j].n_totalt_arbetslosa =
+					json.Blad1[i].alla_total/100 * alla_lan[j].total_befolkning;
+				
+
+				alla_lan[j].n_man_arbetslosa =
+					json.Blad1[i]["alla_män"]/100 * alla_lan[j].total_befolkning/2;
+
+				alla_lan[j].n_kvinnor_arbetslosa =
+					json.Blad1[i].alla_kvinnor/100 * alla_lan[j].total_befolkning/2;
+
+				alla_lan[j].n_unga_arbetslosa =
+					json.Blad1[i].ung_total/100 * alla_lan[j].ung_befolkning ;
+
+				alla_lan[j].n_unga_arbetslosa =
+					json.Blad1[i]["ung_män"]/100 * alla_lan[j].ung_befolkning/2;
+				
+				alla_lan[j].n_unga_arbetslosa =
+					json.Blad1[i].ung_kvinnor/100 * alla_lan[j].ung_befolkning/2;
+
+			}
+		}
+	}
+	
+}
+
 
 
